3e552d6085d6010bd4ddfcbd04d28e22b0ff0d80
joddiepasche <joddie.pasche@gmail.com>
2020-05-06 16:06:36 +0200

    premiere construction de montagne

--- js/sketch.js
+++ js/sketch.js
@@ -4,34 +4,34 @@
 let timer = 1;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  rectMode(CENTER);
+    createCanvas(window.innerWidth, window.innerHeight);
+    rectMode(CENTER);
 }
 
 function draw() {
-  // Conditionellle nous permettant de savoir si la souris est appuyée 
-  // et si le timer est écoulé
-  if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
-    // Appel de notre fonction generator
-    generator(width / 2, height / 2, height-150);
-  }
+    // Conditionellle nous permettant de savoir si la souris est appuyée
+    // et si le timer est écoulé
+    if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
+        // Appel de notre fonction generator
+        generator(width / 2, height / 2, height - 150);
+    }
 }
 
 // Fonction avec quatres paramètres
 function generator(x, y, size) {
-  // fill utilise le paramètre de notre fonction
-  for (let offset = size; offset > 0; offset -= 10) { 
-      fill(random(0,230));
-      rect(x, y, offset, offset);
-      console.log(offset);
-  }
+    // fill utilise le paramètre de notre fonction
+    for (let offset = size; offset > 0; offset -= 20) {
+        fill(random(0, 230));
+        triangle(x, y, width, height, 0, height, offset)
+        console.log(offset);
+    }
 }
 
-function mousePressed() { 
-  // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
-  save();
+function mousePressed() {
+    // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
+    save();
 }
 
-function windowResized() { 
+function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
